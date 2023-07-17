446ece4976a488a4478f4cdd6aae44b61a1ec7b2
Jean Gabriel Frusca <jeang96@gmail.com>
2023-07-17 20:57:26 -0300

    feat: implementação completa do quarto desafio

--- quarto-desafio/impressora.java
+++ quarto-desafio/impressora.java
@@ -1,8 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class impressora {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        List<String> left = new ArrayList<>();
+        List<String> right = new ArrayList<>();
+
 
         int linesNumber = input.nextInt();
         input.nextLine();
@@ -16,15 +21,33 @@ public class impressora {
 
         input.close();
 
-
-        System.out.print("[");
         for (int i = 0; i < phrases.length; i++) {
-            System.out.print(phrases[i]);
-            if (i != (phrases.length-1)) {
-                System.out.print(", ");
+            for (int j = 0; j < phrases[i].length()/2;  j++) {
+                String phrase = phrases[i];
+                left.add(Character.toString(phrase.charAt(j)));
             }
+
+            for (int j = phrases[i].length()/2 ; j < phrases[i].length(); j++) {
+                String phrase = phrases[i];
+                right.add(Character.toString(phrase.charAt(j)));
+            }
+
+
+            String leftReversed = "";
+            for (int m = 0; m < left.size(); m ++) {
+                leftReversed = left.get(m) + leftReversed;
+            }
+
+            String rightReversed = "";
+            for (int n = 0; n < right.size(); n++) {
+                rightReversed = right.get(n) + rightReversed;
+            }
+
+
+            System.out.println(leftReversed + rightReversed);
+            right.clear();
+            left.clear();
         }
-        System.out.println("]");
 
     }
 }
